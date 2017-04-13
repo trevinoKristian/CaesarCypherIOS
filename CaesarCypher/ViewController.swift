6e66dc6b871f92f5467d828da07c090f55437728
Kristian Trevino <trevikri@mail.gvsu.edu>
2017-04-13 16:25:08 -0400

    added notfication functionality

--- CaesarCypher/ViewController.swift
+++ CaesarCypher/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 import Foundation
 
+//for notifications
+import UserNotifications
+
 class ViewController: UIViewController {
 
     
@@ -56,6 +59,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //for notifications
+        // Configure User Notification Center
+        UNUserNotificationCenter.current().delegate = self
     }
     
     //starts the timer
@@ -225,6 +232,71 @@ class ViewController: UIViewController {
         
     }
     
+    //-----------------------------------------------------------------------------------
+    //For Notifications
+    
+    
+    @IBAction func triggerButton(_ sender: Any) {
+        // Request Notification Settings
+        UNUserNotificationCenter.current().getNotificationSettings { (notificationSettings) in
+            switch notificationSettings.authorizationStatus {
+            case .notDetermined:
+                self.requestAuthorization(completionHandler: { (success) in
+                    guard success else { return }
+                    
+                    // Schedule Local Notification
+                    self.scheduleLocalNotification()
+                })
+            case .authorized:
+                // Schedule Local Notification
+                self.scheduleLocalNotification()
+            case .denied:
+                print("Application Not Allowed to Display Notifications")
+            }
+        }
+    }
+    
+    private func requestAuthorization(completionHandler: @escaping (_ success: Bool) -> ()) {
+        // Request Authorization
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
+            if let error = error {
+                print("Request Authorization Failed (\(error), \(error.localizedDescription))")
+            }
+            
+            completionHandler(success)
+        }
+    }
+    
+    private func scheduleLocalNotification() {
+        // Create Notification Content
+        let notificationContent = UNMutableNotificationContent()
+        
+        // Configure Notification Content
+        notificationContent.title = "Alarm"
+        notificationContent.subtitle = "CIS 357 group"
+        notificationContent.body = "Play game to turn off alarm!"
+        
+        // Add Trigger
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        
+        // Create Notification Request
+        let notificationRequest = UNNotificationRequest(identifier: "cocoacasts_local_notification", content: notificationContent, trigger: notificationTrigger)
+        
+        // Add Request to User Notification Center
+        UNUserNotificationCenter.current().add(notificationRequest) { (error) in
+            if let error = error {
+                print("Unable to Add Notification Request (\(error), \(error.localizedDescription))")
+            }
+        }
+    }
+    
 
 }
 
+extension ViewController: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert])
+    }
+    
+}
